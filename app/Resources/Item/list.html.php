20321046de6d67a754efb7fa01db6aebd808884c
joel hartmann <joelhartmann@hotmail.com>
2020-05-12 08:35:52 +0200

    Login
    Started with LogIn

--- app/Resources/Item/list.html.php
+++ app/Resources/Item/list.html.php
@@ -4,10 +4,8 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
     <title>Einkaufsliste</title>
 </head>
 <body>
@@ -15,6 +13,7 @@
     <div class="row">
         <div class="col-md-12">
             <h1>Einkaufsliste</h1>
+            <h1>Einkaufsliste - <?php echo $username; ?></h1>
 
             <table class="table">
                 <tr>
@@ -24,7 +23,6 @@
                     <th>Bearbeiten</th>
                     <th>Löschen</th>
                 </tr>
-
                 <?php
                     foreach ($items as $item) {
                         ?>
@@ -32,14 +30,13 @@
                             <td><?php echo $item[0] ?></td>
                             <td><?php echo $item[1] ?></td>
                             <td><?php echo $item[2] ?></td>
-                            <td><a href="/list?edit=<?php echo $item[0] ?>" class="btn btn-info">Bearbeiten</a></td>
-                            <td><a href="/delete/<?php echo $item[0] ?>" class="btn btn-danger">X</a></td>
+                            <td><a href="/edit?id=<?php echo $item[0] ?>" class="btn btn-info">Bearbeiten</a></td>
+                            <td><a href="/delete?id=<?php echo $item[0] ?>" class="btn btn-danger">X</a></td>
                         </tr>
                         <?php
                     }
                 ?>
             </table>
-
             <form action="/add" method="post">
                 <div class="form-group">
                     <label for="count">Anzahl</label>
@@ -47,15 +44,15 @@
                 </div>
                 <div class="form-group">
                     <label for="name">Name</label>
-                    <input type="number" class="form-control" id="name" name="name">
+                    <input type="text" class="form-control" id="name" name="name">
                 </div>
                 <button type="submit" class="btn btn-primary">Hinzufügen</button>
             </form>
 
+            <a class="btn btn-danger" href="/logout">Abmelden</a>
         </div>
     </div>
 </div>
-
 <!-- Optional JavaScript -->
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
